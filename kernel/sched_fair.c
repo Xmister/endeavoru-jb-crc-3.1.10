0c6134313a2d29e119624f3f8bad8a8c2bb4b131
Vincent Guittot <vincent.guittot@linaro.org>
2011-12-12 20:21:08 +0100

    sched: Ensure cpu_power periodic update
    With a lot of small tasks, the softirq sched is nearly never called
    when no_hz is enabled. In this case load_balance() is mainly called
    with the newly_idle mode which doesn't update the cpu_power.
    
    Add a next_update field which ensure a maximum update period when
    there is short activity.
    
    Having stale cpu_power information can skew the load-balancing
    decisions, this is cured by the guaranteed update.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1323717668-2143-1-git-send-email-vincent.guittot@linaro.org
    
    Conflicts:
    	kernel/sched_fair.c
    
    Signed-off-by: Xmister <xmisterhu@gmail.com>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -91,6 +91,7 @@ unsigned int __read_mostly sysctl_sched_shares_window = 10000000UL;
 
 static const struct sched_class fair_sched_class;
 
+static unsigned long __read_mostly max_load_balance_interval = HZ/10;
 #if defined(CONFIG_BEST_TRADE_HOTPLUG)
 DEFINE_PER_CPU(struct bthp_lb, bthp_lb) = {0};
 
@@ -2694,6 +2695,11 @@ static void update_group_power(struct sched_domain *sd, int cpu)
 	struct sched_domain *child = sd->child;
 	struct sched_group *group, *sdg = sd->groups;
 	unsigned long power;
+	unsigned long interval;
+
+	interval = msecs_to_jiffies(sd->balance_interval);
+	interval = clamp(interval, 1UL, max_load_balance_interval);
+	sdg->sgp->next_update = jiffies + interval;
 
 	if (!child) {
 		update_cpu_power(sd, cpu);
@@ -2801,12 +2807,15 @@ static inline void update_sg_lb_stats(struct sched_domain *sd,
 	 * domains. In the newly idle case, we will allow all the cpu's
 	 * to do the newly idle load balance.
 	 */
-	if (idle != CPU_NEWLY_IDLE && local_group) {
-		if (balance_cpu != this_cpu) {
-			*balance = 0;
-			return;
-		}
-		update_group_power(sd, this_cpu);
+	if (local_group) {
+		if (idle != CPU_NEWLY_IDLE) {
+			if (balance_cpu != this_cpu) {
+				*balance = 0;
+				return;
+			}
+			update_group_power(sd, this_cpu);
+		} else if (time_after_eq(jiffies, group->sgp->next_update))
+			update_group_power(sd, this_cpu);
 	}
 
 	/* Adjust by relative CPU power of the group */
@@ -3923,8 +3932,6 @@ void select_nohz_load_balancer(int stop_tick)
 #endif
 
 static DEFINE_SPINLOCK(balancing);
-
-static unsigned long __read_mostly max_load_balance_interval = HZ/10;
 
 /*
  * Scale the max load_balance interval with the number of CPUs in the system.
