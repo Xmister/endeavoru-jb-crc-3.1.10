bfd57d5ad2332eaf8397d58ee29ad0d843e1c34b
Adrian Ulrich <adrian@blinkenlights.ch>
2013-02-06 19:30:49 +0100

    multitouch fixed

--- drivers/net/wireless/compat-wireless_R5.SP2.03/include/linux/compat_autoconf.h
+++ drivers/net/wireless/compat-wireless_R5.SP2.03/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * 五  1月 25 23:03:36 CST 2013 
+ * Wed Feb  6 19:28:44 CET 2013 
  * compat-wireless-2.6: ol_R5.00.14
  * linux-2.6: ol_R5.00.21-23-g618c7e5
  */
