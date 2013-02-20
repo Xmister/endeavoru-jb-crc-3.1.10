16347b267026a3f21cfe48a8674896e8c95dbe14
n3ocort3x <root@n3ocort3x-tuxbook.(none)>
2013-02-20 20:07:42 +0100

    fix mess, sry :p

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -98,12 +98,7 @@ struct switch_dev compositesdev = {
 	.print_state = print_switch_state,
 };
 static	char *envp[3] = {"SWITCH_NAME=htcctusbcmd",
-			"SWITCH_STATE=Capture", 0};#ifdef YES_WE_WANT_THE_OS_TYPE_CHECK	123
-			                /* Both board_mfg_mode() and usb_autobot_mode() return 0 if the	124
-			                   device is booted normally.	125
-			                   This check will cause a different USB interface to be set for	126
-			                   Linux which is not wanted. Disable it to get UMS back.	127
-			                */
+			"SWITCH_STATE=Capture", 0};
 
 static struct work_struct cdusbcmdwork;
 static void ctusbcmd_do_work(struct work_struct *w)
@@ -125,12 +120,14 @@ static void composite_request_reset(struct work_struct *w)
 		if (usb_autobot_mode() || board_mfg_mode()) return;
 
 		INFO(cdev, "%s\n", __func__);
-#ifdef YES_WE_WANT_THE_OS_TYPE_CHECK	123
-		/* Both board_mfg_mode() and usb_autobot_mode() return 0 if the	124
-		device is booted normally.	125
-		This check will cause a different USB interface to be set for	126
-		Linux which is not wanted. Disable it to get UMS back.	127
-		*/
+
+#ifdef YES_WE_WANT_THE_OS_TYPE_CHECK
+		                /* Both board_mfg_mode() and usb_autobot_mode() return 0 if the
+		                device is booted normally.
+		                 This check will cause a different USB interface to be set for
+		                 Linux which is not wanted. Disable it to get UMS back.
+		             */
+
 		if (os_type == OS_LINUX)
 			fsg_update_mode(1);
 		else
@@ -1800,4 +1797,5 @@ void usb_composite_setup_continue(struct usb_composite_dev *cdev)
 
 	spin_unlock_irqrestore(&cdev->lock, flags);
 }
+
 
